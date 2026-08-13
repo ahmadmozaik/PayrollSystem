f3d35e4958cf272c48629c81eed35680b402523c
Ahmad Mozaik <ahmad.mozaik5656@gmail.com>
2026-08-13 23:47:21 +0300

    Added IPayable interface and Tested classes in main

---
+++ IPayable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSystem
+{
+    public interface IPayable
+    {
+        //to be defined later in FullTimeEmployee as it implements the ProcessPayment method from the IPayable interface
+        void ProcessPayment(Money amount);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PayrollSystem;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //EmployeeRole Class Test
+        EmployeeRole role = EmployeeRole.Developer;
+        Console.WriteLine(role);
+
+        //Money struct Test
+        Money firstMoney;
+        firstMoney.Amount = 1500;
+        firstMoney.Currency = "TRY";
+        Money secondMoney;
+        secondMoney.Amount = 500;
+        secondMoney.Currency = "TRY";
+        Money total = firstMoney + secondMoney;
+        Console.WriteLine($"Total: {total.Amount} {total.Currency}");
+        
+        Console.ReadLine();
+    }
+}
