7c9e3f1e7fe51490fedd6cc13d66d93e231fa2bd
ahmadmozaik <ahmad.mozaik5656@gmail.com>
2026-08-13 22:22:16 +0300

    Added Money struct

---
+++ Money.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSystem
+{
+    public struct Money
+    {
+        public decimal Amount;
+        public string Currency;
+    }
+}
