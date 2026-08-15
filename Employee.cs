afa05d1700dd00f3d81b30499326825f005bf442
Ahmad Mozaik <ahmad.mozaik5656@gmail.com>
2026-08-15 22:23:52 +0300

    Added TAX_RATE and BaseSalary with validation

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSystem
+{
+    internal class Employee
+    {
+        public const decimal TAX_RATE = 0.10m; // 10% constant tax rate
+
+        private decimal _baseSalary;
+
+        public decimal BaseSalary
+        {
+            get { return _baseSalary; }
+            set
+            {
+                if (value < 1000)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(BaseSalary), "Base salary cannot be less than 1000.");
+                }
+                _baseSalary = value;
+            }
+        }
+    }
+}
