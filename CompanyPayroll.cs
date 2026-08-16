b83b03f9b76a287179cce08f7811c3fb7da7b956
Ahmad Mozaik <ahmad.mozaik5656@gmail.com>
2026-08-16 17:21:41 +0300

    Add CompanyPayroll array and indexer

---
+++ CompanyPayroll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSystem
+{
+    internal class CompanyPayroll
+    {
+        private FullTimeEmployee[] employees;
+
+        public CompanyPayroll(int size)
+        {
+            employees = new FullTimeEmployee[size];
+        }
+
+        public FullTimeEmployee this[int index]
+        {
+            get {return employees[index];}
+            set {employees[index] = value;}
+        }
+    }
+}
