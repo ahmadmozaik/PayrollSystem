a51ccb08fc5b7a5129e5d709df85d2ed7cce680a
Ahmad Mozaik <ahmad.mozaik5656@gmail.com>
2026-08-16 17:18:31 +0300

    Add PayrollHandler delegate

---
+++ PayrollHandler.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSystem
+{
+    public delegate void PayrollHandler(string message);//To be used for OnSalaryProcessed event in the Payroll class
+}
